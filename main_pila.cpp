4af65311051a0503d023de52845f34376ff30436
nicolemarchani <nicole.marchani@ucsp.edu.pe>
2015-11-18 17:11:12 -0500

    Create main_pila.cpp

---
+++ main_pila.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "alf.cpp"
+using namespace std;
+
+void caratula()
+{
+	cout << "Ingrese una opcion: "<<endl;
+	cout << "1: añadir nodo"<<endl;
+	cout << "2: atender nodo"<<endl;
+	
+}
+int main()
+{
+	Stack<int> mycola;
+	int opcion;
+	int salida =5;
+	while(salida != 0)
+	{
+	caratula();
+	cin >> opcion;
+	if(opcion ==1)	
+		{
+		int datos;
+		cout << "cuantos datos quiere añadir" <<endl;
+		cin>>datos;	
+		for(int i =0;i< datos;i++)
+			{cout << "Ingrese el dato "<< i <<endl;
+			int n;
+			cin>>n;		
+			mycola.push(n);}
+		cout << "la nueva pila es " <<endl;
+		mycola.imprimir();}
+	if(opcion ==2)	
+		{cout << "el dato es:  ";
+		mycola.pop();
+		cout << "la pila acumulada es:  " <<endl;
+		mycola.imprimir();}
+	cout << "Que desea hacer ahora? "<<endl;
+	cout << "1: volver al menu    0: salir"<<endl;
+	cin >> salida;}}
+
+//	cola b;
+	//cola mycola + b;
