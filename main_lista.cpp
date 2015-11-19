027e541f650e65ac8756134010e4a6d7f64d86b1
nicolemarchani <nicole.marchani@ucsp.edu.pe>
2015-11-19 07:20:59 -0500

    Create main_lista.cpp

---
+++ main_lista.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "alf.cpp"
+using namespace std;
+
+void caratula()
+{
+	cout << "Ingrese una opcion: "<<endl;
+	cout << "1: añadir nodo"<<endl;
+	cout << "2: atender nodo"<<endl;
+	
+}
+int main()
+{
+	Lista<int> mylista;
+	int opcion;
+	int salida =5;
+	while(salida != 0)
+	{
+	caratula();
+	cin >> opcion;
+	if(opcion ==1)	
+		{
+		int datos;
+		cout << "cuantos datos quiere añadir" <<endl;
+		cin>>datos;	
+		for(int i =0;i< datos;i++)
+			{cout << "Ingrese el dato "<< i <<endl;
+			int n;
+			cin>>n;		
+			mylista.push(n);}
+		cout << "la nueva pila es " <<endl;
+		mycola.imprimir();}
+	if(opcion ==2)	
+		{cout << "el dato es:  ";
+		mylista.pop();
+		cout << "la pila acumulada es:  " <<endl;
+		mylista.imprimir();}
+	cout << "Que desea hacer ahora? "<<endl;
+	cout << "1: volver al menu    0: salir"<<endl;
+	cin >> salida;}}
